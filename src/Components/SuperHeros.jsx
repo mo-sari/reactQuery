b6911352563c3685c8d8d74ac3989feb21aa8e75
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-19 19:45:22 +0430

    error handling with normal axios and fetch requests

--- src/Components/SuperHeros.jsx
+++ src/Components/SuperHeros.jsx
@@ -4,18 +4,23 @@ import axios from "axios"
 export const SuperHeros = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [data, setData] = useState([])
-
+  const [error,setError] = useState('');
   useEffect(() => {
     axios.get('http://localhost:4000/superheroes').then(res => {
       setData(res.data)
       setIsLoading(false)
+    }).catch(Error=>{
+      setError(Error.message);
+      setIsLoading(false);
     })
   }, [])
 
   if (isLoading) {
     return <h2>Loading...</h2>
   }
-
+  if (error) {
+    return <div>{error}</div>
+  }
   return (
     <>
       <h2>Super Heroes Page</h2>
