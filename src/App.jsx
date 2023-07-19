a4573faaaf91c7d692ecac9e5f79a7cb57094394
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-19 20:01:46 +0430

    how to setup ReactQueryDevtools

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,36 @@ import { SuperHeros } from "./Components/SuperHeros";
 import { RqSuperHeros } from "./Components/RqSuperHeros";
 import "./App.css";
 import { QueryClientProvider,QueryClient } from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools"
 const queryClient = new QueryClient();
 const App = () => {
   return <QueryClientProvider client={queryClient}>
 
-   <Router>
-    <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='superheros'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='rqsuperheros'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
+    <Router>
+      <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='superheros'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='rqsuperheros'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
 
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="superheros" element={<SuperHeros/>}/>
-        <Route path="rqsuperheros" element={<RqSuperHeros/>}/>
-          
-        </Routes>
-      </div>
-  </Router>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="superheros" element={<SuperHeros/>}/>
+          <Route path="rqsuperheros" element={<RqSuperHeros/>}/>
+            
+          </Routes>
+        </div>
+    </Router>
+    <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
   </QueryClientProvider>
 };
 export default App;
