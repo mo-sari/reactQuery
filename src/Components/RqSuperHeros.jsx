8e8c512b7863a806c29dab8b24362b7128b6e3b9
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-19 19:39:32 +0430

    basic data fetching with react-query

--- src/Components/RqSuperHeros.jsx
+++ src/Components/RqSuperHeros.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import {useQuery} from 'react-query';
+import axios from 'axios';
 export const RqSuperHeros = () => {
+  //useQuery accepts at least two arguments which
+  //the first one is the id for the query and the
+  //second one is a function that returns a promise
+  const {isLoading,data} = useQuery('super-heros',()=>{
+    return axios.get('http://localhost:4000/superheroes');
+  })
+  if (isLoading) {
+    return <h2>Loading ....</h2>
+  }
   return (
-    <div>RqSuperHeros</div>
+    <>
+    <h2>RqSuperHeors</h2>
+    {
+      data?.data.map(hero=>{
+        return <div key={hero.name} >{hero.name}</div>
+      })
+    }
+    </>
   )
 }
