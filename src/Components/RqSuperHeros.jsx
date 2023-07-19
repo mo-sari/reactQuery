7aa8f6656a63f3afef960f9255666195ffdfbcb5
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-19 19:40:35 +0430

    this is a more common pattern for the fetching

--- src/Components/RqSuperHeros.jsx
+++ src/Components/RqSuperHeros.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import {useQuery} from 'react-query';
 import axios from 'axios';
 export const RqSuperHeros = () => {
-  //useQuery accepts at least two arguments which
-  //the first one is the id for the query and the
-  //second one is a function that returns a promise
-  const {isLoading,data} = useQuery('super-heros',()=>{
+  const fetchData = ()=>{
     return axios.get('http://localhost:4000/superheroes');
-  })
+  }
+  const {isLoading,data} = useQuery('super-heros',fetchData)
   if (isLoading) {
     return <h2>Loading ....</h2>
   }
