fbab9ed985da7742bc85f7c4e3647323871ea931
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-19 19:21:42 +0430

    add some pages and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+import { HomePage } from "./Components/HomePage";
+import { SuperHeros } from "./Components/SuperHeros";
+import { RqSuperHeros } from "./Components/RqSuperHeros";
+import "./App.css";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  return <Router>
+    <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='superheros'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='rqsuperheros'>RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="superheros" element={<SuperHeros/>}/>
+        <Route path="rqsuperheros" element={<RqSuperHeros/>}/>
+          
+        </Routes>
+      </div>
+  </Router>
 };
 export default App;
---
+++ src/Components/RqSuperHeros.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RqSuperHeros = () => {
+  return (
+    <div>RqSuperHeros</div>
+  )
+}
