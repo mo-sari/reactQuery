8382df17447bb317930b4f8fa11e38eb081168d1
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-22 18:25:13 +0430

    polling data on Intervals

--- src/Components/RqSuperHeros.jsx
+++ src/Components/RqSuperHeros.jsx
@@ -5,20 +5,18 @@ export const RqSuperHeros = () => {
   const fetchData = ()=>{
     return axios.get('http://localhost:4000/superheroes');
   }
-  const {isLoading,data,isError,error,isFetching} = useQuery('super-heros',fetchData,{
-    //based on the value you provie to the below property
-    //it fetches or doesn't fetch whenever you Mount to the page.
-    //the default value is true for this property.
-    refetchOnMount:false,
-    //if you change the data in database the old axios way will not 
-    //change the value on the windows until you reload the page and
-    //do the fetching again, but the below property will make sure 
-    //anytime your tab loses focus and regains that, a background
-    //refetch will be done.
-    //the default value is true for this property.
-    //this will make sure your value on screen is in sync with the
-    //actual data in database.
-    refetchOnWindowFocus:true
+  const {isLoading,data,isError,error,isFetching} = useQuery('super-heros'
+  ,fetchData,{
+    //the first property below makes your component refetch 
+    //in an specific period of time without user doing anything
+    //the default value for this property is false but you can
+    //set a number 
+    //first property doesn't work when the web page loses focus
+    //but using the second property you can make sure the refecthing
+    //on intervals is happending even when the page loses focus
+    //you would use these properties when your data changes every now and then
+    refetchInterval:4000,
+    refetchIntervalInBackground:true
   })
   console.log(isLoading,isFetching);
   if (isLoading) {
